2aa2e7007c6a7c8dc14b2f04f1389942e24c489b
student <popihacvova@gmail.com>
2022-10-29 11:24:29 +0300

    update f 3

--- Lab_2.py
+++ Lab_2.py
@@ -1,7 +1,7 @@
-def fac(n):
-    if n == 0:
-        return 1
-    return fac(n - 1) * n
-
-
-print(fac(4))
+def max (a,b):
+    return max(a,b)
+res = max_num(5,10)
+print res:
+    print_rec(3,4)
+except Exception as ex:
+print('Eror information {ex}')
